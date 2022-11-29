20426a43519e1a92ca94b3fa02f581cf49e6184f
Rule Lulu Damara <rulelulu833@gmail.com>
2022-11-29 21:13:50 +0700

    TryGit

--- Main.java
+++ Main.java
@@ -2,22 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package application;
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
+//package application;
 
 
-public class Main extends Application{
+public class Main{
 
         public static void main(String[] args){
-        launch();
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        System.out.println("Hello Java");
     }
 }
